153ca5f93e3c15a9bf1b1f7c6e26b03827bddaf2
Sruthi Veeragandham <sruthiv123@berkeley.edu>
2019-03-11 02:33:50 -0700

    Small change to copying in shell scripts

--- part2/cpu-sanity.sh
+++ part2/cpu-sanity.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cp mem.circ tests/circ_files
-cp ../part1/alu.circ tests/circ_files
-cp ../part1/regfile.circ tests/circ_files
+cp ../part1/alu.circ alu.circ 
+cp alu.circ tests/circ_files
+cp ../part1/regfile.circ regfile.circ
+cp regfile.circ tests/circ_files
 cp cpu.circ tests/circ_files
 cd tests/circ_files
 ./sanity_test.py
--- part2/cpu-user.sh
+++ part2/cpu-user.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cp mem.circ my_tests/circ_files
-cp ../part1/alu.circ my_tests/circ_files
-cp ../part1/regfile.circ my_tests/circ_files
+cp ../part1/alu.circ alu.circ 
+cp ../part1/regfile.circ regfile.circ
+cp alu.circ my_tests/circ_files
+cp regfile.circ my_tests/circ_files
 cp cpu.circ my_tests/circ_files
 cd my_tests/circ_files
 ./modular_test.py
